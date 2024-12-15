1b70f6f908f24556c1d521a19924ee9b2352bb42
André <andrearaujomti@gmail.com>
2024-12-15 19:28:08 -0300

    added createIssue file

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost',
-    pageLoadTimeout: 180000
+    pageLoadTimeout: 3000000
     //defaultCommandTimeout: 180000
   },
   fixturesFolder: false,
--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -2,6 +2,7 @@
   Cypress.Commands.add('login', (
     user = Cypress.env('user_name'),
     password = Cypress.env('user_password'),
+    { cacheSession = true } = {},
   ) => {
     const login = () => {
       cy.visit('/users/sign_in')
@@ -11,8 +12,23 @@
       cy.get("[data-qa-selector='sign_in_button']").click()
     }
   
-    login()
-  }) 
+    const validate = () => {
+      cy.visit('/')
+      cy.location('pathname', { timeout: 4000 })
+        .should('not.eq', '/users/sign_in')
+    }
+  
+    const options = {
+      cacheAcrossSpecs: true,
+      validate,
+    }
+  
+    if (cacheSession) {
+      cy.session(user, login, options)
+    } else {
+      login()
+    }
+  })
 } // login
 
   {
@@ -21,4 +37,25 @@
       cy.contains('Sign out').click()
     })
   } // logout
+
+  {
+    Cypress.Commands.add('gui_createProject', project => {
+      cy.visit('/projects/new')
+    
+      cy.get('#project_name').type(project.name)
+      cy.get('#project_description').type(project.description)
+      cy.get('.qa-initialize-with-readme-checkbox').check()
+      cy.contains('Create project').click()
+    })
+  } // Create project
+
+  {
+    Cypress.Commands.add('gui_createIssue', issue => {
+      cy.visit(`/${Cypress.env('user_name')}/${issue.project.name}/issues/new`)
+    
+      cy.get('.qa-issuable-form-title').type(issue.title)
+      cy.get('.qa-issuable-form-description').type(issue.description)
+      cy.contains('Submit issue').click()
+    })
+  } // Create issue
   
